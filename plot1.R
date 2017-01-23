509b0337aa3ac47d25ffb6693969b972aeaa7f8e
PiyushDeshmukh <p.desh31@gmail.com>
2017-01-23 22:48:41 +0530

    generated plot-1 histogram

---
+++ plot1.R
@@ -0,0 +1,12 @@
+hpc <- read.csv('../household_power_consumption.txt', na.strings = '?', sep = ';', nrows = -1)
+hpc['Date'] <-lapply(hpc['Date'], strptime, "%d/%m/%Y")
+hpc <- hpc[(hpc$Date >= '2007-02-01' & hpc$Date <= '2007-02-02'),]
+
+
+if (file.exists('plot1.png'))
+{
+    file.remove('plot1.png')
+}
+png('plot1.png', width = 480, height = 480, bg = 'transparent')
+hist(hpc[['Global_active_power']], col = 'red', xlab = 'Global Active Power (kilowatts)', main = 'Global Active Power', breaks = 11)
+dev.off(which = dev.cur())
